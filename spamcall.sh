613c3968a0d28c75affb98e9f83830efb77eaadb
PROGRAMMER <79271699+mrvirusspm@users.noreply.github.com>
2021-08-03 18:26:27 +0700

    Add files via upload

---
+++ spamcall.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# code warna
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+sleep 1
+echo '\033[33;1m'
+figlet Welcome
+echo '\033[36;1m'"౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼"
+echo '\033[36;1m'"౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼"$yellow
+figlet SpamCall
+echo '\033[36;1m'"౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼"
+echo '\033[36;1m'"౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼"
+echo "\033[32;1m        Spam\033[32;1mCall\033[32;1m B\033[32;1my Pa\033[32;1mng\033[32;1mlim\033[32;1ma Ja\033[32;1mte\033[32;1mn\033[32;1mg\033[32;1m"
+echo $red"      ================================"
+echo
+sleep 1
+    echo $red"{"$white"1"$red"}"$white" Subc"$white"ri"$white"be"$white" Ce"$white"nel"$white" Au"$white"th"$white"or"
+    echo $red"{"$white"2"$red"}"$white" Start"$white" Spam"$white"Call"
+    echo $red"{"$white"3"$red"}"$white" Keluar"
+    echo $cyan"౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼౼"$white
+read -p "Pilih: " panglimajatengpalingganteng
+if [ $panglimajatengpalingganteng = 1 ]; then
+# "donplay"
+xdg-open https://youtube.com/channel/UCSJohuQCtqfD2P73Z65g8jg
+sh spamcall.sh
+fi
+if [ $panglimajatengpalingganteng = 2 ]; then
+git clone https://github.com/projectsku/projects2
+clear
+sleep 2
+    toilet -f big -F gay PrankCall
+    echo '\033[37;1m'"+"'\033[36;1m'"================================================"'\033[37;1m'"+"
+    echo '\033[36;1m'"{"'\033[35;1m'"•"$cyan"} "'\033[37;1m'"Au"'\033[37;1m'"t"'\033[37;1m'"ho"'\033[37;1m'"r = Pa"'\033[37;1m'"ngli"'\033[37;1m'"ma Jat"'\033[37;1m'"eng"
+    echo '\033[36;1m'"{"'\033[35;1m'"•"$cyan"} "'\033[37;1m'"Team   = Mafia Teknologi Indonesia"
+    echo '\033[36;1m'"{"'\033[35;1m'"•"$cyan"} "'\033[37;1m'"Kontak = +62 882-1235-2864"
+    echo '\033[37;1m'"+"'\033[36;1m'"================================================"'\033[37;1m'"+"
+    echo '\033[36;1m'"{"'\033[35;1m'"×"$cyan"}"'\033[37;1m'" Gunakan Dengan Bijak"
+    echo '\033[36;1m'"{"'\033[35;1m'"√"$cyan"}"'\033[37;1m'" Jangan Lupa subrek"
+    echo "+"'\033[36;1m'"================================================"'\033[37;1m'"+"$red
+    read -p "Mulai Ketik ( Y / N ) = " mulio
+fi
+if [ $mulio = y ] || [ $mulio = Y ]; then
+cd projects2
+python spam.py
+fi
+if [ $panglimajatengpalingganteng = 3 ]; then
+figlet babay
+exit
+fi
+if [ $mulio = n ] || [ $muluo = N ]; then
+echo "tanks"
+exit
+fi
